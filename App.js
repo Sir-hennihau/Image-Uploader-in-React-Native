bae33810cfbd9453a61f0ec2ae02b1c4b7009af8
Sir <hendrik.hausen@mail.de>
2017-07-30 17:34:25 +0200

    Can now upload a single image from a static uri, still alot of chaoscode, but wanted to back the progress up. Codecleanup and functionality improvements will follow.

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   PixelRatio,
   TouchableOpacity,
   Image,
+  Button,
 } from 'react-native';
 
 import ImagePicker from 'react-native-image-picker';
@@ -37,12 +38,37 @@ export default class App extends React.Component {
         console.log('User tapped custom button: ', response.customButton);
       } else {
         const source = { uri: response.uri };
+        // fetch needs the source as a string, "source" is counted as an object
+        const sourceAsString = JSON.stringify(source);
 
         // You can also display the image using data:
         // let source = { uri: 'data:image/jpeg;base64,' + response.data };
 
+        // Change state to uri
         this.setState({
           avatarSource: source,
+        });
+        console.log('State set.');
+
+        // Creating new FormData
+        const data = new FormData();
+
+        console.log('Source URI is: ', sourceAsString);
+        data.append('data', {
+          uri: 'file:///storage/emulated/0/Android/data/com.imageuploader/files/Pictures/image-e526b6a1-65d8-4ab7-8f39-5520976a7112.jpg',
+          type: 'image/jpeg', // or photo.type
+          name: 'datei.jpeg',
+        });
+        data.append('name', 'testName'); // you can append anyone.
+
+        // fetch to url
+        fetch('http://hendrikhausen.com/hidden/phpupload/upload.php', {
+          method: 'post',
+          body: data,
+        }).then((res) => {
+          console.log(res);
+        }).catch((error) => {
+          console.log('Network error is: ', error);
         });
       }
     });
@@ -66,6 +92,7 @@ export default class App extends React.Component {
       } else if (response.customButton) {
         console.log('User tapped custom button: ', response.customButton);
       } else {
+         // Change state to uri
         this.setState({
           videoSource: response.uri,
         });
@@ -100,15 +127,17 @@ export default class App extends React.Component {
         <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
           <View style={[styles.avatar, styles.avatarContainer, { marginBottom: 20 }]}>
             { this.state.avatarSource === null ? <Text>Select a Photo</Text> :
-              <Image style={styles.avatar} source={this.state.avatarSource} />
+            <Image style={styles.avatar} source={{ uri: 'file:///storage/emulated/0/Android/data/com.imageuploader/files/Pictures/image-e526b6a1-65d8-4ab7-8f39-5520976a7112.jpg' }} />
             }
           </View>
         </TouchableOpacity>
-
+        <Button
+          title="ASD"
+        />
         <TouchableOpacity onPress={this.selectVideoTapped.bind(this)}>
           <View style={[styles.avatar, styles.avatarContainer]}>
             { this.state.videoSource === null ? <Text>Select a Video</Text> :
-              <Text>{ this.state.videoSource }</Text>
+            <Text>{ this.state.videoSource }</Text>
             }
 
           </View>
